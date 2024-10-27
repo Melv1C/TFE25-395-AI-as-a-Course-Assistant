10e0c1ae0ff7a4a7af158d5c3566bdef41fa0dd2
Melv1C <melvyn.claes@student.uclouvain.be>
2024-10-27 15:40:46 +0100

    feat(run): :sparkles: update run file with the new package release

--- inginious_run_file/run.py
+++ inginious_run_file/run.py
@@ -1,21 +1,30 @@
+import yaml
 from ai_course_assistant import AICourseAssistant, AIFeedbackBlock
 
 from inginious_container_api import feedback, input
 
-URL = "https://example.com/get_feedback"
+URL = "https://example.com"
+
+with open("task.yaml", "r") as f:
+    task = yaml.safe_load(f)
+    print(task)
+
 
 if __name__ == "__main__":
-    question = open("question.txt", "r").read()
+    question = task["context"]
     input_student = input.get_input("code")
 
-    feedback.set_global_result("success")
-    feedback.set_grade(100)
+    feedback.set_global_result("failed")
+    feedback.set_grade(0)
     feedback.set_global_feedback("Some basic feedback")
 
     AICourseAssistant.init(URL)
     assistant = AICourseAssistant(question, input_student)
-    res = assistant.ask_feedback()
+    res = assistant.getFeedbackSync()
     if res.success:
-        feedback.set_global_feedback(AIFeedbackBlock(res.message), True)
+        feedback.set_global_feedback(AIFeedbackBlock(res.feedback), True)
+    else:
+        print("Error:", res.message)
+        feedback.set_global_feedback("An error occurred while asking for an AI feedback.", True)
 
         
